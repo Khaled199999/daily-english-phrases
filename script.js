043718fc196a556fab461b3b641d9c0677aa3ccd
Khaled199999 <kldloub@yahoo.com>
2025-06-29 15:02:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,5 @@
 let currentIndex = 0;
 
-function showPhrase(index) {
-  document.getElementById('en-text').innerText = phrases[index].en;
-  document.getElementById('ar-text').innerText = phrases[index].ar;
-}
-
-function speak(text) {
-  const utterance = new SpeechSynthesisUtterance(text);
-  utterance.lang = 'en-US';
-  speechSynthesis.speak(utterance);
-}
-
-function speakCurrent() {
-  speak(phrases[currentIndex].en);
-}
-
-function nextPhrase() {
-  if (currentIndex < phrases.length - 1) {
-    currentIndex++;
-    showPhrase(currentIndex);
-  }
-}
-
-function prevPhrase() {
-  if (currentIndex > 0) {
-    currentIndex--;
-    showPhrase(currentIndex);
-  }
-}
-
-function repeatPhrase() {
-  speakCurrent();
-}
-
-// إظهار أول جملة عند التحميل
-showPhrase(currentIndex);
-
-let currentIndex = 0;
-
 // استدعاء المفضلة من التخزين المحلي أو إنشاء مصفوفة جديدة
 let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
 
